cc01c770da822a0a6ca5816d0a1f8de2f9aae737
Jmartw <146871238+Jmartw@users.noreply.github.com>
2023-10-20 14:36:33 -0400

    took out department

--- src/test/java/com/pluralsight/Product.java
+++ src/test/java/com/pluralsight/Product.java
@@ -1,34 +1,14 @@
 package com.pluralsight;
 
 public class Product {
-    private String name;
     private String id;
+    private String name;
     private  double price;
-    private String department;
 
-    public Product(String name, String id, double price, String department) {
+    public Product(String id, String name, double price) {
+        this.id = id;
         this.name = name;
-        this.id = String.valueOf(id);
         this.price = price;
-        this.department = department;
-    }
-
-    @Override
-    public String toString() {
-        return "Product{" +
-                "name='" + name + '\'' +
-                ", id='" + id + '\'' +
-                ", price=" + price +
-                ", department='" + department + '\'' +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getId() {
@@ -39,6 +19,14 @@ public class Product {
         this.id = id;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getPrice() {
         return price;
     }
@@ -46,13 +34,6 @@ public class Product {
     public void setPrice(double price) {
         this.price = price;
     }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+}
 
 }
